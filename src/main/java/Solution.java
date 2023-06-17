431d15e272fcbc6a861489a2fce34813ad302d86
alek <wait82985@gmail.com>
2023-06-17 14:04:53 +0600

    problem solved. Improve memoization using 3D array instead of map

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -4,30 +4,30 @@ import java.util.Map;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-        Map<String, Integer> memo = new HashMap<>();
-        return dfs(prices, true, 0, 0, memo);
+        Integer[][][] memo = new Integer[2][2][100_002];
+        return dfs(prices, 1, 0, 0, memo);
     }
 
-    private int dfs(int[] prices, boolean ableToBuy, int numberOfTransactions, int index, Map<String, Integer> memo ) {
+    private int dfs(int[] prices, int ableToBuy, int numberOfTransactions, int index, Integer[][][] memo ) {
         if (numberOfTransactions == 2 || index == prices.length) {
             return 0;
         }
-        if(memo.containsKey(ableToBuy + " " + numberOfTransactions + " " + index)){
-            return memo.get(ableToBuy + " " + numberOfTransactions + " " + index);
+        if(memo[ableToBuy][numberOfTransactions][index] != null){
+            return memo[ableToBuy][numberOfTransactions][index];
         }
 
         int result = 0;
-        if (ableToBuy) {
-            int doNothing = dfs(prices, true, numberOfTransactions, index + 1, memo);
-            int buy = dfs(prices, false, numberOfTransactions, index + 1, memo) - prices[index];
+        if (ableToBuy == 1) {
+            int doNothing = dfs(prices, 1, numberOfTransactions, index + 1, memo);
+            int buy = dfs(prices, 0, numberOfTransactions, index + 1, memo) - prices[index];
             result = Math.max(doNothing, buy);
         }
-        if (!ableToBuy) {
-            int doNothing = dfs(prices, false, numberOfTransactions, index + 1, memo);
-            int sell = prices[index] + dfs(prices, true, numberOfTransactions + 1, index + 1, memo);
+        if (ableToBuy == 0) {
+            int doNothing = dfs(prices, 0, numberOfTransactions, index + 1, memo);
+            int sell = prices[index] + dfs(prices, 1, numberOfTransactions + 1, index + 1, memo);
             result = Math.max(doNothing, sell);
         }
-        memo.put(ableToBuy + " " + numberOfTransactions + " " + index, result);
+        memo[ableToBuy][numberOfTransactions][index] = result;
         return result;
     }
 }
