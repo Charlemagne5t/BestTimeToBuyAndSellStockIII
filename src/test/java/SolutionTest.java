d05dcec9b7af80265c6839935b2ce57acaf78e18
alek <wait82985@gmail.com>
2023-06-17 13:33:38 +0600

    initial commit. 3 test cases

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void maxProfitTest1() {
+        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        int output = 6;
+        Assert.assertEquals(output, new Solution().maxProfit(prices));
+    }
+
+    @Test
+    public void maxProfitTest2() {
+        int[] prices = {1, 2, 3, 4, 5};
+        int output = 4;
+        Assert.assertEquals(output, new Solution().maxProfit(prices));
+    }
+
+    @Test
+    public void maxProfitTest3() {
+        int[] prices = {1, 2, 3, 4, 5};
+        int output = 0;
+        Assert.assertEquals(output, new Solution().maxProfit(prices));
+    }
+}
