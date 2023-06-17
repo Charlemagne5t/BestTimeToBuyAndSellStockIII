710b469bea6c02c9330054a9be42d7959d5eea76
alek <wait82985@gmail.com>
2023-06-17 13:57:20 +0600

    214/214 tests passed but not accepted with TLE

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,26 +1,33 @@
+import java.util.HashMap;
+
+import java.util.Map;
+
 public class Solution {
     public int maxProfit(int[] prices) {
-
-        return dfs(prices, true, 0, 0);
+        Map<String, Integer> memo = new HashMap<>();
+        return dfs(prices, true, 0, 0, memo);
     }
 
-    private int dfs(int[] prices, boolean ableToBuy, int numberOfTransactions, int index) {
+    private int dfs(int[] prices, boolean ableToBuy, int numberOfTransactions, int index, Map<String, Integer> memo ) {
         if (numberOfTransactions == 2 || index == prices.length) {
             return 0;
+        }
+        if(memo.containsKey(ableToBuy + " " + numberOfTransactions + " " + index)){
+            return memo.get(ableToBuy + " " + numberOfTransactions + " " + index);
         }
 
         int result = 0;
         if (ableToBuy) {
-            int doNothing = dfs(prices, true, numberOfTransactions, index + 1);
-            int buy = dfs(prices, false, numberOfTransactions, index + 1) - prices[index];
+            int doNothing = dfs(prices, true, numberOfTransactions, index + 1, memo);
+            int buy = dfs(prices, false, numberOfTransactions, index + 1, memo) - prices[index];
             result = Math.max(doNothing, buy);
         }
         if (!ableToBuy) {
-            int doNothing = dfs(prices, false, numberOfTransactions, index + 1);
-            int sell = prices[index] + dfs(prices, true, numberOfTransactions + 1, index + 1);
+            int doNothing = dfs(prices, false, numberOfTransactions, index + 1, memo);
+            int sell = prices[index] + dfs(prices, true, numberOfTransactions + 1, index + 1, memo);
             result = Math.max(doNothing, sell);
         }
-
+        memo.put(ableToBuy + " " + numberOfTransactions + " " + index, result);
         return result;
     }
 }
